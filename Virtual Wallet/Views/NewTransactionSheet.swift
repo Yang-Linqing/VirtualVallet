936fa2f6b8713f29f5f2a64146e1694325869264
杨林青 <yang-linqing@outlook.com>
2022-11-19 16:28:06 +0800

    将部分视图提取为 Swift Package

--- Virtual Wallet/Views/NewTransactionSheet.swift
+++ Virtual Wallet/Views/NewTransactionSheet.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import MyViewLibrary
 
 struct NewTransactionSheet: View {
     // MARK: Environment Variables
--- Virtual Wallet/Views/TransactionEditorView.swift
+++ Virtual Wallet/Views/TransactionEditorView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import MyViewLibrary
 
 struct TransactionEditorView: View {
     @Binding var transaction: Transaction
