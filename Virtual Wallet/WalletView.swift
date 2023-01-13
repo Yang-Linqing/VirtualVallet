5ff22cb177d552b62c309cdab7fe014f0b00fa02
杨林青 <yang-linqing@outlook.com>
2023-01-13 16:04:45 +0800

    Revert "压缩选项使用自定义表单实现"
    This reverts commit 4a7358e642f73502c7661b50953b8143db47ca1c.

--- Virtual Wallet/WalletView.swift
+++ Virtual Wallet/WalletView.swift
@@ -12,7 +12,6 @@ struct WalletView: View {
     @Binding var wallet: Wallet
     @State private var selection = Set<UUID>()
     @Environment(\.editMode) private var editMode
-    @State private var showSquashSheet = false
     
     var body: some View {
         List(selection: $selection) {
@@ -31,65 +30,6 @@ struct WalletView: View {
                 }
             }
         }
-        .sheet(isPresented: $showSquashSheet) {
-            VStack {
-                HStack {
-                    Text("如何压缩？")
-                        .bold()
-                    Spacer()
-                }
-                Button {
-                    squashSelectedByType()
-                    showSquashSheet = false
-                } label: {
-                    HStack(spacing: 0.0) {
-                        VStack(alignment: .leading) {
-                            Text("合并同类项").bold()
-                            Text("将相同类型交易金额汇总到每类最近一笔上。")
-                                .multilineTextAlignment(.leading)
-                        }
-                        .padding(.trailing)
-                        Spacer()
-                        Image(systemName: "arrow.triangle.pull")
-                            .resizable()
-                            .frame(width: 25.0, height: 50.0)
-                        Image(systemName: "arrow.triangle.pull")
-                            .resizable()
-                            .frame(width: 25.0, height: 50.0)
-                    }
-                }
-                .buttonCard()
-                Button(role: .destructive) {
-                    squashSelected()
-                    showSquashSheet = false
-                } label: {
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text("压缩为一个").bold()
-                            Text("将所有交易金额汇总到最近一笔上。")
-                                .multilineTextAlignment(.leading)
-                        }
-                        .padding(.trailing)
-                        Spacer()
-                        Image(systemName: "arrow.triangle.merge")
-                            .resizable()
-                            .frame(width: 40.0, height: 50.0)
-                    }
-                }
-                .buttonCard()
-                Button {
-                    showSquashSheet = false
-                } label: {
-                    Text("取消").frame(maxWidth: .infinity)
-                }
-                .buttonCard()
-                .padding(.top)
-                Spacer()
-            }
-            .padding()
-            .actionSheetBackground()
-            .presentationDetents([.medium])
-        }
         .navigationTitle($wallet.name)
         .toolbar {
             ToolbarItemGroup(placement: .bottomBar) {
@@ -104,8 +44,9 @@ struct WalletView: View {
                     }
                     .tint(.red)
                     .disabled(selection.isEmpty)
-                    Button {
-                        showSquashSheet = true
+                    Menu {
+                        Button("合并同类项", action: squashSelectedByType)
+                        Button("压缩为一个", action: squashSelected)
                     } label: {
                         Label("压缩", systemImage: "archivebox")
                             .labelStyle(.titleAndIcon)
@@ -118,6 +59,17 @@ struct WalletView: View {
                         }
                     } label: {
                         Label("添加交易", systemImage: "plus.circle.fill")
+                            .labelStyle(.titleAndIcon)
+                    }
+                    Menu {
+                        Button(action: squashSelectedByType) {
+                            Label("合并同类项", systemImage: "list.number")
+                        }
+                        Button(role: .destructive,action: squashSelected) {
+                            Label("压缩为一个", systemImage: "arrow.triangle.merge")
+                        }
+                    } label: {
+                        Label("压缩", systemImage: "archivebox")
                             .labelStyle(.titleAndIcon)
                     }
                 }
@@ -180,23 +132,6 @@ fileprivate let dateFormatter: DateFormatter = {
     formatter.timeStyle = .none
     return formatter
 }()
-
-fileprivate extension View {
-    func actionSheetBackground() -> some View {
-        self.background {
-            Color(uiColor: .systemGroupedBackground)
-                .ignoresSafeArea()
-        }
-    }
-    func buttonCard() -> some View {
-        self.padding()
-            .background {
-                RoundedRectangle(cornerRadius: 16, style: .continuous)
-                    .fill(Color(uiColor: .secondarySystemGroupedBackground))
-            
-        }
-    }
-}
 
 struct WalletView_Previews: PreviewProvider {
     @State static var wallet = Wallet.sampleSet.randomElement()!
